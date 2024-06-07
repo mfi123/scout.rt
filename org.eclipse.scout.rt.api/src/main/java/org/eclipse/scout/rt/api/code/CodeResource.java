bc6e2cf21059ecd8c533f5c132345f5cca03bc3b
Fritz Schinkel <fritz.schinkel@bsi-software.com>
2024-06-07 10:06:17 +0200

    CodeResource: remove duplicate translations
    Translations are inherited from language-only-locales to locales with a
    location. It is therefore not necessary to send the same translation
    for dependent locales.
    Examples:
    * {de=groß, de_DE=groß} => {de=groß}
    * {de=groß, de_CH=gross} => {de=groß, de_CH=gross}
    * {de=ok, en=ok} => {de=ok, en=ok}

--- org.eclipse.scout.rt.api/src/main/java/org/eclipse/scout/rt/api/code/CodeResource.java
+++ org.eclipse.scout.rt.api/src/main/java/org/eclipse/scout/rt/api/code/CodeResource.java
@@ -11,12 +11,14 @@ package org.eclipse.scout.rt.api.code;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.function.BiConsumer;
 
 import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
@@ -31,6 +33,8 @@ import org.eclipse.scout.rt.api.data.code.IApiExposedCodeTypeContributor;
 import org.eclipse.scout.rt.platform.BEANS;
 import org.eclipse.scout.rt.platform.context.RunContexts;
 import org.eclipse.scout.rt.platform.nls.NlsLocale;
+import org.eclipse.scout.rt.platform.util.CollectionUtility;
+import org.eclipse.scout.rt.platform.util.ObjectUtility;
 import org.eclipse.scout.rt.platform.util.StringUtility;
 import org.eclipse.scout.rt.rest.IRestResource;
 import org.slf4j.Logger;
@@ -94,12 +98,13 @@ public class CodeResource implements IRestResource {
       return; // no other languages available
     }
 
-    for (Locale otherLanguage : otherAppLanguages) {
-      RunContexts.copyCurrent()
-          .withLocale(otherLanguage)
-          .call(this::getCodeTypesById).values()
-          .forEach(otherCodeTypeDo -> mergeCodeTypeTexts(otherCodeTypeDo, codeTypeMap));
-    }
+    otherAppLanguages.stream()
+        // order by language tag, locales without location come first ([de, en_US, de_DE, en, en_GB] -> [de, de_DE, en, en_GB, en_US])
+        .sorted(Comparator.comparing(Locale::toLanguageTag))
+        .forEachOrdered(otherLanguage -> RunContexts.copyCurrent()
+            .withLocale(otherLanguage)
+            .call(this::getCodeTypesById).values()
+            .forEach(otherCodeTypeDo -> mergeCodeTypeTexts(otherCodeTypeDo, codeTypeMap)));
   }
 
   protected void mergeCodeTypeTexts(CodeTypeDo from, Map<String, CodeTypeDo> targetMap) {
@@ -110,8 +115,8 @@ public class CodeResource implements IRestResource {
     if (target == null) {
       return;
     }
-    from.getTexts().forEach(target::withText);
-    from.getTextsPlural().forEach(target::withTextPlural);
+    mergeTexts(from.getTexts(), target.getTexts(), target::withText);
+    mergeTexts(from.getTextsPlural(), target.getTextsPlural(), target::withTextPlural);
     Collection<CodeDo> targetChildren = getChildCodes(target);
     from.getCodes().forEach(code -> mergeCodeTexts(code, targetChildren));
   }
@@ -127,9 +132,42 @@ public class CodeResource implements IRestResource {
     if (target == null) {
       return;
     }
-    from.getTexts().forEach(target::withText);
+    mergeTexts(from.getTexts(), target.getTexts(), target::withText);
     Collection<CodeDo> targetChildren = getChildCodes(target);
     from.getChildren().forEach(code -> mergeCodeTexts(code, targetChildren));
+  }
+
+  /**
+   * Merges the to text maps (languageTag to text). If there is already an entry in the target map with the same text
+   * and a languageTag that is a prefix of the entry in the from map, this entry is skipped as this text is considered
+   * to be inherited. <br>
+   * Examples:
+   * <ul>
+   * <li>merge {de_DE=groß} into {de=groß} => {de=groß}</li>
+   * <li>merge {de_CH=gross} into {de=groß} => {de=groß, de_CH=gross}</li>
+   * <li>merge {de=ok} into {en=ok} => {de=ok, en=ok}</li>
+   * </ul>
+   */
+  protected void mergeTexts(Map<String /* languageTag */, String /* text */> fromTexts, Map<String /* languageTag */, String /* text */> targetTexts, BiConsumer<String /* languageTag */, String /* text */> withText) {
+    if (CollectionUtility.isEmpty(fromTexts)) {
+      // no fromTexts -> nothing to merge
+      return;
+    }
+
+    if (CollectionUtility.isEmpty(targetTexts)) {
+      // no targetTexts -> copy everything
+      fromTexts.forEach(withText);
+      return;
+    }
+
+    fromTexts.entrySet()
+        .stream()
+        .filter(entry -> targetTexts.entrySet().stream()
+            // check if there is already an entry with a languageTag that is a prefix to the current one ...
+            .noneMatch(targetEntry -> StringUtility.startsWith(entry.getKey(), targetEntry.getKey())
+                // ... and the same text
+                && ObjectUtility.equals(entry.getValue(), targetEntry.getValue())))
+        .forEach(entry -> withText.accept(entry.getKey(), entry.getValue()));
   }
 
   protected Collection<CodeDo> getChildCodes(CodeTypeDo parent) {
